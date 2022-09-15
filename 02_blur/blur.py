8ee3cb7335e95276a9cd6f04c6e9e7726d8209f6
Teo <teovs.wch@gmail.com>
2022-09-15 18:22:51 -0300

    Revisão

--- 02_blur/blur.py
+++ 02_blur/blur.py
@@ -24,30 +24,6 @@ def blurIngenuo(img, fullw):
 
     return img_return
 
-def chadBlur(img, fullw):
-    w = fullw//2
-
-    #img_return = np.array
-    img_return = np.copy (img)
-    print (img_return.dtype)
-    #img_return = img.reshape((img.shape[0], img.shape[1], 1))
-
-    for y in range(w, len(img)-w):
-        soma = 0
-        for x in range(w, len(img[0])-w):
-            if (x == w):                
-                for i in range(-w, w+1):
-                    for j in range(-w, w+1):
-                        soma += img[y+i, w+j]
-            else:
-                for k in range(-w, w+1):
-                    soma += img[y+k, x+w]
-                    soma -= img[y+k, x-w-1]
-            media = soma / (fullw**2)
-            img_return[y, x] = media
-
-    return img_return
-
 def blurSeparavel(img, fullW):
     halfW = fullW // 2
 
@@ -64,7 +40,6 @@ def blurSeparavel(img, fullW):
 
     img_return = np.copy(img_horizontal)
 
-    # return img_horizontal
     for y in range(halfW, len(img)-halfW):
         for x in range(halfW, len(img[0])-halfW):
             soma = 0
@@ -157,9 +132,9 @@ def blurIntegral(img, fullW):
 
 
 def main ():
-    ingenuo = False
+    ingenuo = True
     separavel = True
-    integral = False
+    integral = True
 
     # Leitura do arquivo-----------------------------------
     # img = cv2.imread(INPUT_IMAGE, cv2.IMREAD_GRAYSCALE)
@@ -179,19 +154,19 @@ def main ():
         img_output = blurIngenuo(img, 9)
         cv2.imwrite('04 - blurIngenuo.png', img_output*255)
         print ('Tempo ingênuo: %f' % (timeit.default_timer () - start_time))
-        cv2.imshow('Output', img_output)
+        cv2.imshow('Ingenuo', img_output)
     if separavel:
         start_time = timeit.default_timer ()
         img_output = blurSeparavel(img, 15)
         cv2.imwrite('04 - blurSeparavel.png', img_output*255)
         print ('Tempo separável: %f' % (timeit.default_timer () - start_time))
-        cv2.imshow('Output', img_output)
+        cv2.imshow('Separavel', img_output)
     if integral:
         start_time = timeit.default_timer ()
         img_output = blurIntegral(img, 55) 
         cv2.imwrite('04 - blurIntegral.png', img_output*255)
         print ('Tempo integral: %f' % (timeit.default_timer () - start_time))
-        cv2.imshow('Output', img_output)
+        cv2.imshow('Integral', img_output)
 
     # cv2.imshow('Output', img_output)
     cv2.waitKey ()
